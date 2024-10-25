19ba4f2bffd747d4ce5a60f91443a08c3419c6c1
noolfj <firdavsjuraev8@gmail.com>
2024-10-25 15:04:54 +0500

    Удалил Push уведомл

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
 import 'package:crm_task_manager/api/service/api_service.dart';
-import 'package:crm_task_manager/api/service/firebase_api.dart';
 import 'package:crm_task_manager/bloc/auth_domain/domain_bloc.dart';
 import 'package:crm_task_manager/bloc/history/history_bloc.dart';
 import 'package:crm_task_manager/bloc/lead/lead_bloc.dart';
@@ -10,44 +8,17 @@ import 'package:crm_task_manager/bloc/region/region_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:crm_task_manager/firebase_options.dart';
 import 'screens/auth/auth_screen.dart';
 import 'screens/auth/login_screen.dart';
-import 'screens/home_screen.dart'; // Главный экран
-import 'screens/chats/chats_screen.dart'; // Экран уведомлений
-
-// Обработчик фоновых push-сообщений
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print('Handling a background message: ${message.messageId}');
-  print('Message data: ${message.data}');
-  print('Received a message while in the foreground!');
-
-  // Дополнительно выводим данные уведомления
-  print('Notification title: ${message.notification?.title}');
-  print('Notification body: ${message.notification?.body}');
-}
-  final navigatorKey = GlobalKey<NavigatorState>();
+import 'screens/home_screen.dart';  // Главный экран
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // // Обязательно инициализируем Flutter binding, чтобы плагин мог корректно работать
+  // WidgetsFlutterBinding.ensureInitialized();
 
-  // Инициализация Firebase с конфигурацией для текущей платформы
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-// Получаем FCM-токен
-  String? fcmToken = await FirebaseMessaging.instance.getToken();
-  if (fcmToken != null) {
-    print('FCM-токен: $fcmToken');
-
-    // Отправляем FCM-токен на сервер
-    ApiService apiService = ApiService();
-    await apiService.sendDeviceToken(fcmToken);
-  } else {
-    print('Не удалось получить FCM-токен');
-  }
+  // // // Пример инициализации плеера (опционально, если требуется инициализация заранее)
+  // final AudioPlayer audioPlayer = AudioPlayer();
 
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
@@ -56,12 +27,7 @@ void main() async {
       systemNavigationBarColor: Colors.white,
     ),
   );
-
-  // Инициализация Firebase API для push-уведомлений
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-  FirebaseApi firebaseApi = FirebaseApi();
-  await firebaseApi.initNotifications();
-
+  
   runApp(MyApp());
 }
 
@@ -70,7 +36,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
-
         BlocProvider(
           create: (context) => DomainBloc(ApiService()),
         ),
@@ -97,7 +62,6 @@ class MyApp extends StatelessWidget {
           '/': (context) => AuthScreen(),  // Экран для проверки домена
           '/login': (context) => LoginScreen(),  // Экран логина
           '/home': (context) => HomeScreen(),  // Главный экран после успешного входа
-          '/notification_screen': (context) => ChatsScreen(), // Экран уведомлений
         },
       ),
     );
--- lib/screens/chats/chats_screen.dart
+++ lib/screens/chats/chats_screen.dart
@@ -1,127 +1,31 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:crm_task_manager/api/service/api_service.dart';
-import 'package:crm_task_manager/api/service/firebase_api.dart';
 import 'package:crm_task_manager/models/chats_model.dart';
 import 'package:crm_task_manager/screens/chats/chats_items.dart';
 import 'package:crm_task_manager/screens/chats/chat_sms_screen.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
-class ChatsScreen extends StatefulWidget {
-  @override
-  _ChatsScreenState createState() => _ChatsScreenState();
-}
-
-class _ChatsScreenState extends State<ChatsScreen> {
-  final ApiService apiService = ApiService();
+class ChatsScreen extends StatelessWidget {
+  final ApiService apiService = ApiService(); // Инициализация ApiService
   bool isNavigating = false;
-
-  @override
-  void initState() {
-    super.initState();
-    print('Инициализация ChatsScreen');
-    _initializeFirebase();
-  }
-
-  void _initializeFirebase() async {
-    await FirebaseApi().initNotifications();
-    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-      print('Получено сообщение на переднем плане!');
-      final chatId = message.data['chatId'];
-
-      if (chatId != null) {
-        print('Получено уведомление для чата с id: $chatId');
-        
-        // Передаем контекст и chatId
-        checkChatExists(context, chatId).then((chatExists) async {
-          if (chatExists) {
-            print('Чат с id $chatId найден. Открываем его.');
-            ChatItem? chatItem = await _buildChatItemById(int.parse(chatId)); // Используем await для получения результата Future
-            if (chatItem != null) {
-              apiService.getMessages(int.parse(chatId)).then((messages) {
-                if (mounted) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ChatSmsScreen(
-                        chatId: int.parse(chatId),
-                        chatItem: chatItem,
-                        messages: messages,
-                      ),
-                    ),
-                  );
-                }
-              });
-            } else {
-              print('Ошибка: chatItem равен null.');
-            }
-          } else {
-            print('Чат с id $chatId не найден в списке.');
-          }
-        });
-      } else {
-        print('chatId отсутствует в данных уведомления.');
-      }
-    });
-
-    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
-      final chatId = message.data['chatId'];
-
-      if (chatId != null) {
-        print('Открытие приложения через уведомление для чата с id: $chatId');
-        
-        // Передаем контекст и chatId
-        checkChatExists(context, chatId).then((chatExists) async {
-          if (chatExists) {
-            print('Чат с id $chatId найден. Открываем его.');
-            var chatItem = await _buildChatItemById(int.parse(chatId));
-            var messages = await apiService.getMessages(int.parse(chatId));
-            if (mounted) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => ChatSmsScreen(
-                    chatId: int.parse(chatId),
-                    chatItem: chatItem!,
-                    messages: messages,
-                  ),
-                ),
-              );
-            }
-          } else {
-            print('Чат с id $chatId не найден в списке.');
-          }
-        }).catchError((error) {
-          print('Ошибка при проверке существования чата: $error');
-        });
-      } else {
-        print('chatId отсутствует в данных уведомления.');
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: FutureBuilder<List<Chats>>(
-        future: apiService.getAllChats(),
+        future: apiService
+            .getAllChats(), // Использование метода для получения чатов
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           } else if (snapshot.hasError) {
-            return Center(child: Text('Ошибка загрузки чатов: ${snapshot.error}'));
+            return Center(
+                child: Text('Ошибка загрузки чатов: ${snapshot.error}'));
           } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
             return const Center(child: Text('Нет доступных чатов'));
           }
 
           final List<Chats> chats = snapshot.data!;
-          for (var chat in chats) {
-            print('ID чата: ${chat.id}, Имя: ${chat.name}');
-          }
-
           return ListView.builder(
             itemCount: chats.length,
             itemBuilder: (context, index) {
@@ -130,7 +34,8 @@ class _ChatsScreenState extends State<ChatsScreen> {
                 onTap: () async {
                   if (!isNavigating) {
                     isNavigating = true;
-                    final messages = await apiService.getMessages(chat.id);
+                    final messages = await apiService.getMessages(chat
+                        .id); // Использование метода для получения сообщений
                     Navigator.push(
                       context,
                       MaterialPageRoute(
@@ -141,7 +46,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
                         ),
                       ),
                     ).then((_) {
-                      isNavigating = false; // Сбрасываем флаг навигации
+                      isNavigating = false;
                     });
                   }
                 },
@@ -156,11 +61,11 @@ class _ChatsScreenState extends State<ChatsScreen> {
 
   ChatItem _buildChatItem(Chats chat) {
     return ChatItem(
-      name: chat.name,
-      message: chat.lastMessage,
-      time: "08:00", // Здесь можно заменить на актуальное время
-      avatar: "assets/images/AvatarChat.png",
-      icon: _mapChannelToIcon(chat.channel),
+      chat.name,
+      chat.lastMessage,
+      "08:00",
+      "assets/images/AvatarChat.png",
+      _mapChannelToIcon(chat.channel),
     );
   }
 
@@ -174,58 +79,4 @@ class _ChatsScreenState extends State<ChatsScreen> {
     };
     return channelIconMap[channel] ?? 'assets/icons/leads/default.png';
   }
-
-  Future<ChatItem?> _buildChatItemById(int chatId) async {
-    final chat = await apiService.getChatById(chatId);
-    if (chat != null) {
-      return ChatItem(
-        name: chat.name,
-        message: chat.lastMessage,
-        time: "08:00",
-        avatar: "assets/images/AvatarChat.png",
-        icon: _mapChannelToIcon(chat.channel),
-      );
-    } else {
-      print('Чат с id $chatId не найден.');
-      return null;
-    }
-  }
-
-  Future<bool> checkChatExists(BuildContext context, String chatId) async {
-    final List<Chats> chats = await apiService.getAllChats();
-    print('Полученный chatId: $chatId');
-    print('Список id чатов: ${chats.map((chat) => chat.id).toList()}');
-
-    for (var chat in chats) {
-      if (chat.id == int.parse(chatId)) {
-        print('Найден чат с id: $chatId');
-
-        // Получение сообщений чата
-        final messages = await apiService.getMessages(chat.id);
-
-        // Переход на экран с сообщениями чата
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => ChatSmsScreen(
-              chatItem: _buildChatItem(chat),
-              messages: messages,
-              chatId: chat.id,
-            ),
-          ),
-        );
-
-        return true;
-      }
-    }
-    print('Чат с id $chatId не найден.');
-    return false;
-  }
-}
-
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  print('Обработка фонового сообщения: ${message.messageId}');
-  print('Данные сообщения: ${message.data}');
-  print('Заголовок уведомления: ${message.notification?.title}');
-  print('Текст уведомления: ${message.notification?.body}');
 }
