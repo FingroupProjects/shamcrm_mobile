564ed64dada87381263c56cec4347c38a86b573d
noolfj <firdavsjuraev8@gmail.com>
2024-12-27 10:33:14 +0500

    Доб сообщение после создание удаления обновл Заметки Сделки Конатк лица

--- lib/screens/lead/tabBar/lead_details/contact_person_update_screen.dart
+++ lib/screens/lead/tabBar/lead_details/contact_person_update_screen.dart
@@ -105,6 +105,28 @@ class _ContactPersonUpdateScreenState extends State<ContactPersonUpdateScreen> {
               ),
             );
           } else if (state is ContactPersonSuccess) {
+            ScaffoldMessenger.of(context).showSnackBar(
+                   SnackBar(
+                     content: Text(
+                       'Контактное лицо успешно обновлено!',
+                       style: TextStyle(
+                         fontFamily: 'Gilroy',
+                         fontSize: 16, 
+                         fontWeight: FontWeight.w500, 
+                         color: Colors.white, 
+                       ),
+                     ),
+                     behavior: SnackBarBehavior.floating,
+                     margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                     shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(12),
+                     ),
+                     backgroundColor: Colors.green,
+                     elevation: 3,
+                     padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16), 
+                     duration: Duration(seconds: 2),
+                   ),
+                );
             Navigator.pop(context);
           }
         },
