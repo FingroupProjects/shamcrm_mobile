3db92b7f33a244620f795271725e3cd6eb7f6e5c
dilshodjonavezov <dilshodjonavezov@gmail.com>
2025-04-09 16:48:39 +0500

    Подключние API раздел заказа редактированеи удаление перетаскивание, исправление баг

--- lib/page_2/order/order_details/order_column.dart
+++ lib/page_2/order/order_details/order_column.dart
@@ -1,9 +1,13 @@
+import 'package:crm_task_manager/bloc/page_2_BLOC/order_status/order_status_bloc.dart';
+import 'package:crm_task_manager/bloc/page_2_BLOC/order_status/order_status_event.dart';
+import 'package:crm_task_manager/bloc/page_2_BLOC/order_status/order_status_state.dart';
 import 'package:crm_task_manager/custom_widget/animation.dart';
 import 'package:crm_task_manager/models/page_2/order_card.dart';
 import 'package:crm_task_manager/page_2/order/order_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class OrderColumn extends StatelessWidget {
+class OrderColumn extends StatefulWidget {
   final int statusId;
   final String name;
   final String? searchQuery;
@@ -12,7 +16,7 @@ class OrderColumn extends StatelessWidget {
   final int? organizationId;
   final VoidCallback onStatusUpdated;
   final void Function(int newStatusId) onStatusId;
-  final Function(int) onTabChange; // Новый коллбэк
+  final Function(int) onTabChange;
 
   const OrderColumn({
     required this.statusId,
@@ -23,38 +27,116 @@ class OrderColumn extends StatelessWidget {
     this.organizationId,
     required this.onStatusUpdated,
     required this.onStatusId,
-    required this.onTabChange, // Добавляем в конструктор
+    required this.onTabChange,
   });
 
   @override
+  _OrderColumnState createState() => _OrderColumnState();
+}
+
+class _OrderColumnState extends State<OrderColumn> {
+  final ScrollController _scrollController = ScrollController();
+  int _currentPage = 1;
+  bool _isLoadingMore = false;
+  bool _hasMore = true;
+  final int _perPage = 20;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - 200 &&
+        !_isLoadingMore &&
+        _hasMore) {
+      _loadMoreOrders();
+    }
+  }
+
+  void _loadMoreOrders() {
+    final state = context.read<OrderBloc>().state;
+    if (state is OrderLoaded) {
+      final orderBloc = context.read<OrderBloc>();
+      if (orderBloc.allOrdersFetched[widget.statusId] == true) {
+        setState(() {
+          _hasMore = false;
+        });
+        return;
+      }
+
+      setState(() {
+        _isLoadingMore = true;
+        _currentPage++;
+      });
+      context.read<OrderBloc>().add(FetchMoreOrders(
+        statusId: widget.statusId,
+        page: _currentPage,
+        perPage: _perPage,
+      ));
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: isLoading
-              ? const Center(
-                  child: PlayStoreImageLoading(
-                    size: 80.0,
-                    duration: Duration(milliseconds: 1000),
-                  ),
-                )
-              : orders.isEmpty
-                  ? const Center(child: Text('Нет заказов'))
-                  : ListView.builder(
-                      itemCount: orders.length,
-                      itemBuilder: (context, index) {
-                        final order = orders[index];
-                        return OrderCard(
-                          order: order,
-                          organizationId: organizationId ?? order.organizationId,
-                          onStatusUpdated: onStatusUpdated,
-                          onStatusId: onStatusId,
-                          onTabChange: onTabChange, // Передаем коллбэк
-                        );
-                      },
+    return BlocListener<OrderBloc, OrderState>(
+      listener: (context, state) {
+        if (state is OrderLoaded) {
+          final orderBloc = context.read<OrderBloc>();
+          setState(() {
+            _isLoadingMore = false;
+            _hasMore = !(orderBloc.allOrdersFetched[widget.statusId] == true);
+          });
+        }
+      },
+      child: Column(
+        children: [
+          Expanded(
+            child: widget.isLoading
+                ? const Center(
+                    child: PlayStoreImageLoading(
+                      size: 80.0,
+                      duration: Duration(milliseconds: 1000),
                     ),
-        ),
-      ],
+                  )
+                : widget.orders.isEmpty
+                    ? const Center(child: Text('Нет заказов'))
+                    : ListView.builder(
+                        controller: _scrollController,
+                        itemCount: widget.orders.length + (_isLoadingMore ? 1 : 0),
+                        itemBuilder: (context, index) {
+                          if (index == widget.orders.length && _isLoadingMore) {
+                            return const Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: PlayStoreImageLoading(
+                                  size: 80.0,
+                                  duration: Duration(milliseconds: 1000),
+                                ),
+                              ),
+                            );
+                          }
+                          final order = widget.orders[index];
+                          return OrderCard(
+                            order: order,
+                            organizationId: widget.organizationId ?? order.organizationId,
+                            onStatusUpdated: widget.onStatusUpdated,
+                            onStatusId: widget.onStatusId,
+                            onTabChange: widget.onTabChange,
+                          );
+                        },
+                      ),
+          ),
+        ],
+      ),
     );
   }
 }
