eaebd3cc6b5e834902040884b504be842b75623e
noolfj <firdavsjuraev8@gmail.com>
2025-04-14 08:43:26 +0500

    UPDT CODE

--- lib/main.dart
+++ lib/main.dart
@@ -97,9 +97,8 @@ final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async { 
   WidgetsFlutterBinding.ensureInitialized(); 
-  await AppTrackingTransparency.requestTrackingAuthorization();
   final apiService = ApiService();
-    final authService = AuthService();
+  final authService = AuthService();
   final bool isDomainChecked = await apiService.isDomainChecked();
   if (isDomainChecked) {
     await apiService.initialize();
@@ -107,9 +106,13 @@ void main() async {
   final String? token = await apiService.getToken();
   final String? pin = await authService.getPin();
 
+  await AppTrackingTransparency.requestTrackingAuthorization();
+
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,  
   );
+
   await FirebaseMessaging.instance.requestPermission();
   await getFCMTokens(apiService);
 
