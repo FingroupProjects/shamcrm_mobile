705fb04a5e52a7937f21cc5d3f906a39d7ee0491
shahr <shahriyorturayev4@gmail.com>
2025-09-11 16:38:24 +0500

    supplier_model

---
+++ lib/page_2/supplier/supplier_creen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SupplierCreen extends StatefulWidget {
+  const SupplierCreen({super.key});
+
+  @override
+  State<SupplierCreen> createState() => _SupplierCreenState();
+}
+
+class _SupplierCreenState extends State<SupplierCreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
