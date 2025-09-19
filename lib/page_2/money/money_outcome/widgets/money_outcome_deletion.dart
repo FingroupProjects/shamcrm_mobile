03a6519b33d6e99cc0cae38458f9d72cc7ddc07c
macbook <mirxtohir@gmail.com>
2025-09-19 17:30:54 +0500

    add filters to outcome too

--- lib/page_2/money/money_outcome/widgets/money_outcome_deletion.dart
+++ lib/page_2/money/money_outcome/widgets/money_outcome_deletion.dart
@@ -36,27 +36,7 @@ class MoneyOutcomeDeleteDialog extends StatelessWidget {
             ),
           );
         } else if (state is MoneyOutcomeDeleteSuccess) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(
-              content: Text(
-                '${state.message}',
-                style: TextStyle(
-                  fontFamily: 'Gilroy',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.white,
-                ),
-              ),
-              behavior: SnackBarBehavior.floating,
-              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-              backgroundColor: Colors.green,
-              elevation: 3,
-              padding: EdgeInsets.symmetric(vertical: 12, horizontal: 16),
-            ),
-          );
+          // Просто закрываем диалог, сообщение покажет основной экран
           Navigator.of(context).pop(true);
         }
       },
