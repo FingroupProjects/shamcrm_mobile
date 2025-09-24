ce7353321b3e0808af0e2d475a05aa9822f83882
DILSHODJON <dilshodjonavezov>
2025-09-24 16:31:50 +0500

    ff

--- lib/bloc/auth_domain/domain_state.dart
+++ lib/bloc/auth_domain/domain_state.dart
@@ -1,42 +1,48 @@
 import 'package:equatable/equatable.dart';
-import '../../models/domain_check.dart'; 
+import '../../models/domain_check.dart';
 
 abstract class DomainState extends Equatable {
   const DomainState();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
 class DomainInitial extends DomainState {}
 
 class DomainLoading extends DomainState {}
 
-// Новое состояние для успешной верификации email
+// Состояние для успешной проверки email
 class EmailVerified extends DomainState {
   final String login;
-
-  EmailVerified(this.login);
-
+  
+  const EmailVerified(this.login);
+  
   @override
-  List<Object> get props => [login];
+  List<Object?> get props => [login];
 }
 
-// Старые состояния для обратной совместимости
+// Состояние для валидной сессии
+class SessionValid extends DomainState {}
+
+// Состояние для очищенной сессии
+class SessionCleared extends DomainState {}
+
+// Старое состояние для обратной совместимости
 class DomainLoaded extends DomainState {
   final DomainCheck domainCheck;
-
-  DomainLoaded(this.domainCheck);
-
+  
+  const DomainLoaded(this.domainCheck);
+  
   @override
-  List<Object> get props => [domainCheck];
+  List<Object?> get props => [domainCheck];
 }
 
 class DomainError extends DomainState {
   final String message;
-
-  DomainError(this.message);
-
+  
+  const DomainError(this.message);
+  
   @override
-  List<Object> get props => [message];
+  List<Object?> get props => [message];
 }
